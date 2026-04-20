3430f9441d733f200c6bf25eaf4afdb7dd02eddc
MazanovAB1972 <mazalexey@yandex.ru>
2026-04-20 13:53:07 +0300

    Initial commit

--- QuoteApi/api/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify
-from api import app
-from werkzeug.exceptions import HTTPException
-
-
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """ Функция для перехвата HTTP ошибок и возврата в виде JSON."""
-    return jsonify({"error": str(e)}), e.code
--- QuoteApi/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-flask db upgrade
-gunicorn -w 5 -b 0.0.0.0:5005 run:app
