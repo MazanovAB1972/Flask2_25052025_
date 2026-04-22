8abd85bf8569471ce285dba1b7680203aef2907a
MazanovAB1972 <mazalexey@yandex.ru>
2026-04-22 10:53:50 +0300

    Migrate's errors

--- QuoteApi/api/models/author.py
+++ QuoteApi/api/models/author.py
@@ -8,11 +8,13 @@ class AuthorModel(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32), index=True, unique=True)
     surname: Mapped[str] = mapped_column(String(32), index=False, unique=False)
+    surname2: Mapped[str] = mapped_column(String(32), index=False, unique=False)
     quotes: Mapped[list['QuoteModel']] = relationship(back_populates='author', lazy='dynamic')
 
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, surname2):
         self.name = name
         self.surname = surname
+        self.surname2=surname2
 
     def to_dict(self):
         return {"id": self.id, "name": self.name, "surname": self.surname} 
